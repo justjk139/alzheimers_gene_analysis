9fca697a9f7fc8477ca671e6ef895f211160ca13
r1cummin <r1cummin@r1cummin-12070.pods.r1cummin.svc.cluster.local>
2021-02-07 15:06:26 -0800

    Working just need updated report

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ sys.path.insert(0, 'src') #getting the directory of our etl file in order to imp
 from etl import get_file_names, shorten_data, create_path
 #from all_pipeline import fastqc
 from test_pipeline import test_kallisto, test_fastqc
-# from utils import function_to_convert_notebook, function_to_convert_notebook_test
+from utils import function_to_convert_notebook, function_to_convert_notebook_test
 
 
 def main(targets):
@@ -17,6 +17,7 @@ def main(targets):
     if 'all' in targets:
         # HERE is where we run the FULL PIPELINE
         print("This is a placeholder for now")
+        
     if 'data' in targets:
         #doing our data retrieval
         with open('config/data-params.json') as fh:
@@ -26,10 +27,10 @@ def main(targets):
       
     if 'eda' in targets:
         # HERE is where EDA will be implemented
-#         with open('config/eda-params.json') as fh:
-#             eda_cfg = json.load(fh)
-#             function_to_convert_notebook(**eda_cfg)
-        print("This is a placeholder for now")
+        with open('config/eda-params.json') as fh:
+            eda_cfg = json.load(fh)
+            function_to_convert_notebook(**eda_cfg)
+    
     if 'test' in targets:
         
         print("\n\nTest is now running\n")
@@ -42,22 +43,18 @@ def main(targets):
             print("shortening the data from inside Main:")
             fq_1, fq_2 = shorten_data(**test_cfg)
             
+            #fastqc:
             print("running fastqc from main:")
             pass_data, cut_data = test_fastqc([fq_1,fq_2])
             print(pass_data)
             
-#             # cutadapt
-#             print(
-#             cut_fastq_files = cutadapt(pass_data)
-  
-#             print("Running fastqc PART 2 (To reevaluate after data has been trimmed) from main:")
-#             pass_cut_data, fail_cut_data = second_fastqc()
             # kallisto:
             test_kallisto(pass_data)
-            #eda report here:
-#             with open('config/eda-params.json') as fh:
-#                 eda_cfg = json.load(fh)
-#                 function_to_convert_notebook_test(**eda_cfg)
+            
+            #generating the report here:
+            with open('config/eda-params.json') as fh:
+                eda_cfg = json.load(fh)
+                function_to_convert_notebook_test(**eda_cfg)
             
             print("Success!")
         print("\n\nTest finished running\n\n")
--- src/utils.py
+++ src/utils.py
@@ -9,17 +9,17 @@ def function_to_convert_notebook(outdir, report_in_path, report_out_path):
     os.system("mkdir report")
     #print(report_in_path)
     #print(os.listdir())
-    os.system("jupyter nbconvert --to html "+report_in_path)
+    os.system("jupyter nbconvert --to pdf "+ report_in_path)
     #print(os.listdir())
     os.system("mv notebooks/report.html report")
     return
 
 def function_to_convert_notebook_test(outdir, report_in_path, report_out_path):
     os.system("mkdir report")
-    os.system("jupyter nbconvert --to html "+report_in_path)
+    os.system("jupyter nbconvert --to pdf "+ report_in_path)
     #print(os.listdir())
-    os.system("mv notebooks/report.html report")
-    os.rename('report/report.html', 'report/complete_report.html')
+    os.system("mv notebooks/Alzheimers-Biomarker-Analysis.pdf report")
+    #os.rename('report/report.html', 'report/complete_report.html')
     return
 
 # In[ ]:
